7945598bdb650ed7ea220ac67f6fd21e99489054
expbox77 <expbox77@gmail.com>
2022-07-20 00:02:32 +0900

    :D

--- index.js
+++ index.js
@@ -21,12 +21,17 @@ app.get('/', (req, res) => {
   pool.getConnection().then(conn => {
     conn.query("SELECT * FROM data").then((rows) => {
       console.log(rows);
+      res.send("MySQL successfully connected!");
     })
   }).catch(err => {
     //not connected
     console.log("fail!" + err);
-  });
+  })
 });
+
+// app.get('/test', (req, res) =>{
+//   res.send("MySQL successfully connected!");
+// })
 
 app.get('/insert', (req, res) => {
   console.log(req.query.temp);
@@ -37,12 +42,19 @@ app.get('/insert', (req, res) => {
   console.log(req.query.ult);
 
   pool.getConnection().then(conn => {
-
     conn.query("INSERT INTO data (date, temp, humidity, ir1, ir2, flex, ultrasonic) VALUES (now()," + req.query.temp + "," + req.query.humi + "," + req.query.ir1 + "," + req.query.ir2 + "," + req.query.flex + "," + req.query.ult + ")");
+    res.send(`MySQL successfully connected! <br/> <br/>
+    temp : ${req.query.temp} <br/>
+    humi : ${req.query.humi} <br/>
+    ir1 : ${req.query.ir1} <br/>
+    ir2 : ${req.query.ir2} <br/>
+    flex : ${req.query.flex} <br/>
+    ult : ${req.query.ult}<br/> <br/>
+    success!`) 
+    
   }).catch(err => {
     //not connected
     console.log("fail!" + err);
-    res.send("MySQL successfully connected!");
   });
 })
 
